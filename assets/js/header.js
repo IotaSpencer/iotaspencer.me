7da9094d98dc0e1eb5af0f320c3d44b39a6c3cde
Ken Spencer <ken@electrocode.net>
2018-01-15 17:07:49 -0500

    set some more css on ready

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,18 @@
+---
+---
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('shrink');
+
   } else {
     $('nav').removeClass('shrink');
+    $('div.wrapper').css('top', 120);
+
   }
 });
 
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
+  $('div.wrapper').css('top', 120);
+
 });
